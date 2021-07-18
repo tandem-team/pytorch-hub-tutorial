c6e15db15eb4b54450354b16c6cd2ef25e0abbf1
city-of-stars <akhilsk279584@gmail.com>
2021-07-19 02:42:15 +0530

    added checkpoint

--- hubconf.py
+++ hubconf.py
@@ -6,5 +6,8 @@ def squeezenet(pretrained=False, **kwargs):
 	"The statements we write here is displayed when \
 	we call hub.help() on this repository"
 	model = _squeezenet_model(pretrained=pretrained, **kwargs)
+	
+	checkpoint = 'https://download.pytorch.org/models/squeezenet1_0-b66bff10.pth'
+    model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
 	return model
 	
