50beba3b78ad0420b5ebfab1cd6b089a72237bbf
city-of-stars <akhilsk279584@gmail.com>
2021-07-19 01:51:04 +0530

    updated squeezenet import file

--- hubconf.py
+++ hubconf.py
@@ -1,10 +1,10 @@
 #Hub config file
 dependencies = ['torch', 'torchvision']
-from torchvision.models import squeezenet as squeezenet_model
+from torchvision.models import squeezenet1_0 as _squeezenet_model
 
 def squeezenet(pretrained=False, **kwargs):
 	"The statements we write here is displayed when \
 	we call hub.help() on this repository"
-	model = squeezenet_model(pretrained=pretrained, **kwargs)
+	model = _squeezenet_model(pretrained=pretrained, **kwargs)
 	return model
 	
