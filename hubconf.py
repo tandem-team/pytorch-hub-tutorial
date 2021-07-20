7ff89687747f371fc11a85a75870fc56b1fc4dc5
city-of-stars <akhilsk279584@gmail.com>
2021-07-20 23:31:26 +0530

    updated hubconf file

--- hubconf.py
+++ hubconf.py
@@ -2,3 +2,12 @@
 dependencies = ['torch']
 
 from torchvision.models.squeezenet import squeezenet1_0, squeezenet1_1
+
+def squeezenet(pretrained_t=False):
+	"The statements we write here is displayed when \
+	we call hub.help() on this repository"
+	model = squeezenet1_0(pretrained=pretrained_t)
+	
+	#checkpoint = 'https://download.pytorch.org/models/squeezenet1_0-b66bff10.pth'
+	#model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
+	return model
