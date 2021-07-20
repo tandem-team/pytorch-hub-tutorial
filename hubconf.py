810062778b6ee7654e80c82ba143c9d423700606
AKHIL  S <6010712@Mobis.HKMG.Global>
2021-07-20 22:30:34 +0530

    Added some changes in hubconf

--- hubconf.py
+++ hubconf.py
@@ -1,13 +1,15 @@
 #Hub config file
 dependencies = ['torch', 'torchvision']
+import torch
 from torchvision.models import squeezenet1_0 as _squeezenet_model
 
 def squeezenet(pretrained=False, **kwargs):
-	"The statements we write here is displayed when \
-	we call hub.help() on this repository"
+	"""The statements we write here is displayed when \
+	we call hub.help() on this repository"""
 	model = _squeezenet_model(pretrained=pretrained, **kwargs)
 	
 	checkpoint = 'https://download.pytorch.org/models/squeezenet1_0-b66bff10.pth'
 	model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
 	return model
-	
+
+squeezenet()
